549f934de98c19bb723cfa3a5176cbedfe9e2db7
William McNulty <william.mcnulty@live.co.uk>
2016-05-05 23:40:59 +0100

    added unit test project

---
+++ UnitTest/HomeControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
