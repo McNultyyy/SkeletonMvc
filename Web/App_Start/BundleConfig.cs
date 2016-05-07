d1ff5c93ca03519ab242f66963d03acf648538d5
William McNulty <william.mcnulty@live.co.uk>
2016-05-07 19:51:33 +0100

    included jqueryval unobtrusive to the bundle config

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace Web
                         "~/bower_components/jquery/dist/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/bower_components/jquery-validation/dist/jquery.validate.js"));
+                        "~/bower_components/jquery-validation/dist/jquery.validate.js",
+                        "~/bower_components/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
