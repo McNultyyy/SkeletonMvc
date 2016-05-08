b897ff6e70840a89c05f04ace5b9b455cfde8dd2
William McNulty <william.mcnulty@live.co.uk>
2016-05-09 00:08:38 +0100

    build fix for previous commit

--- Model/Entities/Blog.cs
+++ Model/Entities/Blog.cs
@@ -2,18 +2,12 @@
 
 namespace Model.Entities
 {
-    public class Blog: Entity
+    public class Blog : Entity
     {
-        public Blog()
-        {
-            Posts = new List<Post>();
-        }
-
         public override int Id { get; set; }
 
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public virtual IList<Post> Posts { get; set; }
     }
 }
