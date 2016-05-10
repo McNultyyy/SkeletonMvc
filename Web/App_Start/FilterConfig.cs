b8cfd44ab91575dab60cf984a0f891cefd22afbe
William McNulty <william.mcnulty@live.co.uk>
2016-05-10 21:35:24 +0100

    added error pages for 403, 404 and 500

--- Web/App_Start/FilterConfig.cs
+++ Web/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace Web
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
             filters.Add(new HandleConcurrencyExceptionFilter());
         }
     }
---
+++ Web/Controllers/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error/403
+        [ActionName("403")]
+        public ActionResult Forbidden()
+        {
+            Response.StatusCode = 403;
+            return View();
+        }
+
+        // GET: Error/404
+        [ActionName("404")]
+        public ActionResult NotFound()
+        {
+            //return a status code for proper seo
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        // GET: Error/500
+        [ActionName("500")]
+        public ActionResult Error()
+        {
+            //in the global.asax.cs code we handle the error. maybe we can send it to an email.
+            //return a status code for proper seo
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
