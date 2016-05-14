24a9dd8b00f610a904fc3acbf12d4fb6fc5b5ea9
William McNulty <william.mcnulty@live.co.uk>
2016-05-15 00:01:28 +0100

    externalised the build script into a Powershell script

---
+++ Web/build.ps1
@@ -0,0 +1,9 @@
+npm cache clean
+npm install
+
+./node_modules/.bin/bower cache clean
+./node_modules/.bin/bower install
+
+#./node_modules/.bin/gulp build
+
+./node_modules/.bin/rimraf ./node_modules
