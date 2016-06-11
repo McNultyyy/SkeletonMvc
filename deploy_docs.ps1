30e09f0835b434595131364ba5b933f31b84c80f
William McNulty <william.mcnulty@live.co.uk>
2016-06-12 00:58:13 +0100

    actually added commands to deploy_docs

--- deploy_docs.ps1
+++ deploy_docs.ps1
@@ -1,3 +1,7 @@
 cd Documentation/Help
-dir
-pwd
+git config --global user.email $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL
+git config --global user.name $env:APPVEYOR_REPO_COMMIT_AUTHOR
+git clone --branch=gh-pages https://www.github.com/mcnultyyy/SkeletonMvc gh-pages
+git add .
+$message = "CI autocommit from commit "+$env:APPVEYOR_REPO_COMMIT;
+git commit -m $message
