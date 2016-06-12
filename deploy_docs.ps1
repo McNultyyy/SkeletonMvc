5d5eb324e6645c8cf5b3eed8ae35c0e4a7f9901e
william <william.mcnulty@live.co.uk>
2016-06-12 14:22:37 +0100

    externalised the deploy_docs script

--- deploy_docs.ps1
+++ deploy_docs.ps1
@@ -1,8 +1,14 @@
-Set-PSDebug -Trace 1+
-cd Documentation/Help
+cd Documentation
+git config --global credential.helper store
+git config --global core.autocrlf true
+Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GH_TOKEN):x-oauth-basic@github.com`n"
 git config --global user.email $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL
 git config --global user.name $env:APPVEYOR_REPO_COMMIT_AUTHOR
+git config --global push.default simple
 git clone --branch=gh-pages https://www.github.com/mcnultyyy/SkeletonMvc gh-pages
+Copy-Item -Path .\Help\* -Destination .\gh-pages\ -Recurse -Force
+cd gh-pages
 git add .
-$message = "CI autocommit from commit "+$env:APPVEYOR_REPO_COMMIT;
+Set-Variable message ([string]::Format("CI docs deployment from commit {0}", $env:APPVEYOR_REPO_COMMIT))
 git commit -m $message
+git push
