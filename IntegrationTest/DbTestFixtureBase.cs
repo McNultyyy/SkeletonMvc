3d614de0a0b80368965540634da95973645164e4
william <william.mcnulty@live.co.uk>
2016-06-24 22:30:35 +0100

    build fix v2

---
+++ IntegrationTest/DbTestFixtureBase.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.DependencyInjection;
+using Repository.Models.Entities;
+using Repository.Repositorys;
+
+namespace IntegrationTest
+{
+    public class DbTestFixtureBase<TEntity> where TEntity : Entity
+    {
+        public DbTestFixtureBase()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFramework()
+                .AddInMemoryDatabase();
+
+            
+        }
+    }
+}
